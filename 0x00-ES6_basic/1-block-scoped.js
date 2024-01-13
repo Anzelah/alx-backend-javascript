b4de531f1c9715818240ac31fb566d51d0c36fb6
Anzelah <jepkoechanzellah@gmail.com>
2024-01-13 12:41:42 +0000

    lint update

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    var taskInner = true;
-    var task2Inner = false;
+    let taskInner = true;
+    let task2Inner = false;
   }
 
   return [task, task2];
