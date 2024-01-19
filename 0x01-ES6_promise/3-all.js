d51746c7b04d64079401fa0cac7aad6c42422359
Anzelah <jepkoechanzellah@gmail.com>
2024-01-19 19:25:31 +0000

    use all to handle multiple succesful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import {uploadPhoto, createUser} from './utils.js';
+
+export default function handleProfileSignup() {
+  const promise1 = uploadPhoto();
+  const promise2 = createUser();
+  Promise.all([promise1, promise2])
+    .then((result) => {
+      console.log(result[0].body, result[1].firstName, result[1].lastName);
+    }).catch((error) => {
+      console.error('Signup system offline');
+    });
+}
