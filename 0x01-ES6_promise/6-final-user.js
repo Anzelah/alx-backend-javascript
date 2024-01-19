7b62d1ba2898842e5ec644414dfc5aae00c478f3
Anzelah <jepkoechanzellah@gmail.com>
2024-01-19 23:11:18 +0000

    use correct parameters

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,8 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
     .then((res) => res);
 }
