f101b0aedcb2f6319f9075124c99ed23db680433
Anzelah <jepkoechanzellah@gmail.com>
2024-01-19 19:49:36 +0000

    fix error

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const promise1 = uploadPhoto();
-  const promise2 = createUser();
-  Promise.all([promise1, promise2])
+  Promise.all([uploadPhoto(), createUser()])
     .then((result) => {
       console.log(result[0].body, result[1].firstName, result[1].lastName);
     }).catch(() => {
