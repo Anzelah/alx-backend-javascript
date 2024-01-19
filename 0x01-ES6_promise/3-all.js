a3f9ef6b490d4d2c5c9933a485c8526a2ff829e5
Anzelah <jepkoechanzellah@gmail.com>
2024-01-19 20:00:37 +0000

    use template literals to access results

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
     .then((result) => {
-      console.log(result[0].body, result[1].firstName, result[1].lastName);
+      console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
     }).catch(() => {
       console.error(new Error('Signup system offline'));
     });
