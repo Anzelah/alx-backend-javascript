1de734041a7185947afa0fd88b68ee5124ba1671
Anzelah <jepkoechanzellah@gmail.com>
2024-01-19 18:15:03 +0000

    catch me if you can

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  return success
+  const success = true;
+  return promise
     .then((success) => {
+      console.log('Got a response from the API');
       return { status: 200, body: success };
     }).catch((error) => {
       console.error(error);
       return new Error('');
-    }).finally(() => {
-      console.log('Got a response from the API');
     });
 }
