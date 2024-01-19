c4e78295ea90799dd997d1f4aad78b9d0e0f866b
Anzelah <jepkoechanzellah@gmail.com>
2024-01-19 23:09:28 +0000

    add values as parameters n the two functions

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,6 +4,6 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
-    .allSettled([signUpUser(), uploadPhoto()])
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((res) => res);
 }
