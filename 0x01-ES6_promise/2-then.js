a68a4e453c451f6ea1c56afb638a1c393798d2cb
Anzelah <jepkoechanzellah@gmail.com>
2024-01-19 18:05:28 +0000

    there is chaining going on

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,10 @@
-/* eslint-disable no-unused-vars */
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    const success = true;
-    if (success) {
-      const response = { status: 200, body: success };
-      resolve(response);
-    } else {
-      reject(new Error(''));
-    }
-  });
+  return promise
+    .then((success) => {
+      console.log('Got a response from the API');
+      return { status: 200, body: success };
+    }).catch((error) => {
+      console.error(error);
+      return new Error('');
+    });
 }
-
-const p = handleResponseFromAPI();
-p.then((response) => console.log('Got a response from the API'))
-  .catch((error) => console.error('API error:', error));
