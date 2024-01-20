d73c4354ae237ab1a8770755c3274bf12ff4b7a8
Anzelah <jepkoechanzellah@gmail.com>
2024-01-20 18:35:10 +0000

    making code more elegant + lint update

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,15 +1,12 @@
 export default function guardrail(mathFunction) {
-	let queue = [];
-	const success = true;
-	try {
-		let res = mathFunction();
-		if (success) {
-			queue.push(res);
-	}} catch(error) {
-		queue.push(String(error));
-	} finally {
-		queue.push('Guardrail was processed');
-	};
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (error) {
+    queue.push(String(error));
+  } finally {
+    queue.push('Guardrail was processed');
+  }
 
-	return queue;
+  return queue;
 }
