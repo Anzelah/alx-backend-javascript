a2f7613fe2e9faaf6a1b42020f53898f7fedc46b
Anzelah <jepkoechanzellah@gmail.com>
2024-01-23 11:05:20 +0000

    lint update

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,14 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
+  const newArray = students.filter((std) => std.location === city)
+    .map((std) => {
+    const matching = newGrades.find((g) => g.studentId === std.id);
 
-	const newArray = students.filter((std) => std.location === city)
-		.map((std) => {
-		const matching = newGrades.find((g) => g.studentId === std.id);
+    if (matching) {
+      return { ...std, grade: matching.grade }
+    } else {
+      return { ...std, grade: 'N/A' }
+    }
+    });
 
-		
-		if (matching) {
-			return { ...std, grade: matching.grade }
-		} else {
-			return { ...std, grade: 'N/A' }
-		}
-		});
-
-	return newArray;
+  return newArray;
 }
