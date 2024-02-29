bfd10ff2c1f40cb5ac389504ae5bc953b6eee6e5
Anzelah <jepkoechanzellah@gmail.com>
2024-02-29 20:38:09 +0000

    lint update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', function() {
   const data = process.stdin.read();
   if (data !== null) {
-   process.stdout.write('Your name is: ' + data);
+    process.stdout.write('Your name is: ' + data);
   }
 });
 
