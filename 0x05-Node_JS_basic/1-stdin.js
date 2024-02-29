520172f574a157e7120d49495f2e7d99129a674c
Anzelah <jepkoechanzellah@gmail.com>
2024-02-29 20:47:17 +0000

    lint update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
 process.stdin.setEncoding('utf8');
 
-console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('readable', function() {
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
   const data = process.stdin.read();
   if (data !== null) {
     process.stdout.write('Your name is: ' + data);
   }
 });
 
-process.stdin.on('end', function() {
+process.stdin.on('end', () => {
   console.log('This important software is now closing');
 });
