649e8de2d0c5b48fe570b6ef3c434193a08730f5
Anzelah <jepkoechanzellah@gmail.com>
2024-03-01 18:42:43 +0000

    no need to specify url as it takes any endpoint

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,10 +1,8 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  if (req.url === '/' || req.url === '/any_endpoint') {
-    res.write('Hello Holberton School!');
-    res.end();
-  }
+  res.write('Hello Holberton School!');
+  res.end();
 });
 app.listen(1245);
 
