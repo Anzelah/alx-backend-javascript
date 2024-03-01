bcc0151c7cc753e1e88b1d7dd29e4b3fd0fa6306
Anzelah <jepkoechanzellah@gmail.com>
2024-03-01 20:53:56 +0000

    create server using express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.use((request, response) => {
-  response.json({message: 'Hello Holberton School!'});
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
 });
-app.set('port', 1245);
+
+app.listen(1245);
 
 
 module.exports = app;
