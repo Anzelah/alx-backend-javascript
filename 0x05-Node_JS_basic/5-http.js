fd865f1467efd375b271785ca20cd2544866a3d4
Anzelah <jepkoechanzellah@gmail.com>
2024-03-01 19:45:02 +0000

    tbd

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const { readFileAsync } = require('./3-read_file_async');
+const countStudents = require('./2-read_file');
 
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
@@ -7,17 +8,20 @@ const app = http.createServer((req, res) => {
     res.end();
   }
   if (req.url === '/students') {
-    readFileAsync('./database.csv','utf8', (err, data) => {
-      if (err) {
-	console.error('Error reading file');
-        res.write('Internal Server Error');
-        res.end();
-      } else {
-        res.write(`This is the list of our students\n${data}`);
-        res.end();
-      }
+    countStudents(process.argv[2]).then(() => {
+      readFileAsync('./database.csv','utf8', (err, data) => {
+        if (err) {
+	  console.error('Error reading file');
+          res.write('Internal Server Error');
+          res.end();
+        } else {
+	  console.log(data);
+          res.write(`This is the list of our students\n${data.split('\n')}`);
+          res.end();
+        }
     });
-  }
+  });
+}
 });
 app.listen(1245);
 
