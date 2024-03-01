45d4a75c370460b636651088499c0e23bc1a94a6
Anzelah <jepkoechanzellah@gmail.com>
2024-03-01 15:40:10 +0000

    loop through the database.csv

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+function countStudents(path) {  
+  const content = {};
+  const fields = {};
+
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const rows = data.trim().split('\n');
+
+    console.log(`Number of students: ${rows.slice(1).length}`);
+
+    rows.slice(1).forEach((row) => {
+      if (row.trim() !== '') {
+        const values = row.split(',');
+      
+        if (content[values[3]]) {
+          content[values[3]].push(values[0]);
+        } else {
+	  content[values[3]] = [values[0]];
+	}
+
+	if (fields[values[3]]) {
+	  fields[values[3]] += 1;
+	} else {
+	  fields[values[3]] = 1;
+	}
+      }
+    });
+    for (const key of Object.keys(fields)) {
+      if (key !== fields) {
+        console.log(`Number of students in ${key}: ${fields[key]}. List: ${content[key].join(', ')}`)
+      }
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  };
+}
+
+module.exports = countStudents;
