a2c1d4129eb6316fd2c80d7f1f4f9b6d3698cd90
Anzelah <jepkoechanzellah@gmail.com>
2024-03-02 14:36:41 +0000

    students route is working

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -2,31 +2,28 @@ const readDatabase = require('../utils.js');
 
 class StudentsController {
   static getAllStudents(request, response) {
-    readDatabase((err, data) => {
-      if (err) {
-        response.statusCode = 500;
-        response.send('Cannot load the database');
-        console.error(err);
-      } else {
-        response.statusCode = 200;
-        response.send(`This is the list of our students\n${data}`);
-      }
+    readDatabase(process.argv[2]).then((results) => {
+      response.statusCode = 200;
+      response.send(`This is the list of our students\n${results}`);
+    }).catch((err) => {
+      response.statusCode = 500;
+      response.send('Cannot load the database');
+      console.error(err);
     });
   }
   
   static getAllStudentsByMajor(request, response) {
     if (request.params.major !== CS && request.params.major !== SWE) {
       response.status(500).send('Major parameter must be CS or SWE');
-    } else {
-      readDatabase((err, data) => {
-        if (err) {
-          response.status(500).send('Cannot load the database');
-       	  console.error(err);
-        } else {
-          response.status(200).send(`List: ${data}`);
-	}
-      });
+      return;
     }
+
+    readDatabase(process.argv[2]).then((results) => {
+      response.status(200).send(`List: ${results}`);
+    }).catch((err) => {
+      response.status(500).send('Cannot load the database');
+      console.error(err);
+    });
   }
 }
 
